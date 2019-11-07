d289ee3d76c1ee095b6fbd5fbf7d43f947219146
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-07 20:21:42 +0000

    created a get endpoint to fetch shopingList for specified recipes

--- Recipes/recipeRouter.js
+++ Recipes/recipeRouter.js
@@ -1,19 +1,27 @@
 const express = require("express");
 
-const Recipes = require('../Recipes/recipes-Model')
+const Recipes = require("../Recipes/recipes-Model");
 
 const recipeRouter = express.Router();
 
-
-recipeRouter.get('/', (req, res) => {
-    Recipes.getRecipes()
+recipeRouter.get("/", (req, res) => {
+  Recipes.getRecipes()
     .then(recipes => {
-      res.json(recipes)
+      res.json(recipes);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Failed to get recipes' });
+      res.status(500).json({ message: "Failed to get recipes" });
     });
-  });
+});
 
+recipeRouter.get("/:id/shoppingList", (req, res) => {
+  Recipes.getShoppingList(req.params.id)
+    .then(instruction => {
+      res.status(200).json(instruction)
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get an instruction" });
+    });
+});
 
-  module.exports = recipeRouter;
+module.exports = recipeRouter;
--- Recipes/recipes-Model.js
+++ Recipes/recipes-Model.js
@@ -2,14 +2,14 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   getRecipes,
-  getInstructions
+  getShoppingList
 };
 
 function getRecipes() {
   return db("recipes");
 }
 
-function getInstructions(recipe_id) {
+function getShoppingList(recipe_id) {
   return db.select("ingredient_name", "Qty").from("recipes as r")
   .join("combinations as c", "r.id", "=", "c.recipe_id")
   .join("ingredients as i","c.ingredient_id","=","i.id")
