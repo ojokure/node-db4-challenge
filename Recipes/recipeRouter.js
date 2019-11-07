daafac1eaa0444fdcc07d6052ba96704851d5064
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-07 20:03:31 +0000

    created a get endpoint to fetch recipes

--- Recipes/recipeRouter.js
+++ Recipes/recipeRouter.js
@@ -1,6 +1,19 @@
 const express = require("express");
 
+const Recipes = require('../Recipes/recipes-Model')
 
 const recipeRouter = express.Router();
 
-module.exports = recipeRouter;
+
+recipeRouter.get('/', (req, res) => {
+    Recipes.getRecipes()
+    .then(recipes => {
+      res.json(recipes)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get recipes' });
+    });
+  });
+
+
+  module.exports = recipeRouter;
--- Recipes/recipes-Model.js
+++ Recipes/recipes-Model.js
@@ -16,3 +16,14 @@ function getInstructions(recipe_id) {
   .where({"r.id" : recipe_id})
 }
 
+// function findSteps(schemeId) {
+//   return db("schemes as s")
+//     .join("steps as st", "st.step_number", "s.id")
+//     .select("s.scheme_name", "st.instructions", "st.step_number")
+//     .where({ "s.id": schemeId });
+// }
+
+//     select ingredient_name, Qty from recipes as r
+// join combinations as c on r.id = c.recipe_id
+// join ingredients as i on  c.ingredient_id = i.id
+// order by r.id;
